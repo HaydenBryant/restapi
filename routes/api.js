74fa7a194d7aef4958a6708f3671f8a7888901e6
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-04-30 17:26:11 +0000

    lesson 7

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,28 @@
-const express = require('express')
-
+const express = require('express');
 const router = express.Router();
 
-//get a list of ninjas from the database
-router.get('/ninjas', function(req,res){
+// get a list of ninjas from the db
+router.get('/ninjas', function(req, res){
     res.send({type: 'GET'});
 });
 
-//add a new ninja to the db
-router.post('/ninjas', function(req,res){
-    res.send({type: 'POST'});
+// add a new ninja to the db
+router.post('/ninjas', function(req, res){
+    console.log('You made a POST request: ', req.body);
+    res.send({
+        type: 'POST',
+        name: req.body.name,
+        rank: req.body.rank
+    });
 });
 
-//update exisiting ninja in db
-router.put('/ninjas/:id', function(req,res){
+// update a ninja in the db
+router.put('/ninjas/:id', function(req, res){
     res.send({type: 'PUT'});
 });
 
-//delete a ninja from db
-router.delete('/ninjas/:id', function(req,res){
+// delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res){
     res.send({type: 'DELETE'});
 });
 
